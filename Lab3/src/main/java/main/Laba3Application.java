b3bfb224e9ea00c3676ef2a5a060165dd5fcb680
Stepan <zubbodazdeban@mail.ru>
2026-05-02 05:23:37 +0300

    Spring-boot запустился

---
+++ Lab3/src/main/java/main/Laba3Application.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+import javax.swing.*;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author zubbo
+ */
+
+@SpringBootApplication
+public class Laba3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Laba3Application.class, args);
+    }
+}
--- Lab3/src/main/java/main/MissionAnalyzerGUI.java
+++ Lab3/src/main/java/main/MissionAnalyzerGUI.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/*
 package main;
 
 import model.Mission;
@@ -134,3 +136,4 @@ public class MissionAnalyzerGUI extends JFrame {
         }
     }
 }
+*/
